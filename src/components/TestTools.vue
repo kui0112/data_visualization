a67c619824c2c550514243e668cd854f318e4388
TOTORO <kui0112@qq.com>
2024-10-03 18:26:09 +0800

    视频图像混合播放

--- src/components/TestTools.vue
+++ src/components/TestTools.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue"
 import * as service from "../scripts/Service"
+import {useRouter} from "vue-router";
 
+const router = useRouter()
 const object_names = ref<Array<string>>([])
+
+function picturesSwitchDisplayMode(mode: string): void {
+
+}
 
 onMounted(async () => {
   object_names.value = await service.objectNames()
@@ -11,6 +17,13 @@ onMounted(async () => {
 
 <template>
   <div>
+    <div>切换图像/视频/混合</div>
+    <div v-if="router.currentRoute.value.name === 'Pictures'">
+      <a-button @click="picturesSwitchDisplayMode('image')">image</a-button>
+      <a-button @click="picturesSwitchDisplayMode('video')">video</a-button>
+      <a-button @click="picturesSwitchDisplayMode('mixed')">mixed</a-button>
+    </div>
+    <div></div>
     <ul style="list-style-type: circle; margin-top: 5px;">
       <li v-for="object_name in object_names">
         <a id="{{object_name}}"
